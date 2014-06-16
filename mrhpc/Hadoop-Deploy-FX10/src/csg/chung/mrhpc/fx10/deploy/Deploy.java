68645b6f7a9b30a569609e9910b9df7154bb47ec
chung <chung.dao88@gmail.com>
2014-06-16 12:51:23 +0900

    Add Hadoop-Deploy-FX10 project (First commit)

---
+++ mrhpc/Hadoop-Deploy-FX10/src/csg/chung/mrhpc/fx10/deploy/Deploy.java
@@ -0,0 +1,201 @@
+package csg.chung.mrhpc.fx10.deploy;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.InetAddress;
+
+import mpi.MPI;
+import mpi.MPIException;
+
+public class Deploy {
+	/* Don't change the below constants */
+	public final static String HADOOP_TAR_GZ_FILE 		= Configure.DEPLOY_FOLDER + "/hadoop.tar.gz";
+	public final static String DATA_FOLDER 				= Configure.DEPLOY_FOLDER + "/hadoop/data/";
+	public final static String TMP_FOLDER 				= Configure.DEPLOY_FOLDER + "/hadoop/tmp/";
+	public final static String HADOOP_FOLDER 			= Configure.DEPLOY_FOLDER + "/hadoop/code/";
+	
+	/**
+	 * Deploy Hadoop on every node
+	 */
+	public Deploy(){
+		try {
+			int rank = MPI.COMM_WORLD.Rank();
+			
+			// Print node info
+			InetAddress ip = InetAddress.getLocalHost();
+			System.out.println("P" + rank + ": " + ip.getHostName() + " - " + ip.getHostAddress());	
+			
+			if (rank == 0){
+				// Master node
+				initialize();
+				startMaster(rank);
+			}else{
+				startSlaves(rank);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (MPIException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Initialize installation
+	 */
+	public void initialize(){
+		try {
+			runCommand("mkdir " + Configure.DEPLOY_FOLDER + "/hadoop");
+			runCommand("mkdir " + DATA_FOLDER);
+			runCommand("mkdir " + TMP_FOLDER);
+			runCommand("mkdir " + HADOOP_FOLDER);
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Start Namenode and Resource manager
+	 * @param rank Node ID that is responsible for
+	 */
+	public void startMaster(int rank){
+		InetAddress ip;
+		try {
+			ip = InetAddress.getLocalHost();
+			generateCode(rank, ip.getHostAddress());
+			
+			// Start master
+			File folder = new File(Deploy.DATA_FOLDER + rank);
+			if (folder.isDirectory() && folder.list().length == 0){
+				runCommand(Deploy.HADOOP_FOLDER + rank + "/bin/hdfs namenode -format");
+				System.out.println("Format Namenode --> OK");
+			}
+			
+			runCommand(Deploy.HADOOP_FOLDER + rank + "/sbin/hadoop-daemon.sh start namenode");
+			runCommand(Deploy.HADOOP_FOLDER + rank + "/sbin/yarn-daemon.sh start resourcemanager");
+			System.out.println("Start Master --> OK");
+			
+			// Send master address to slave nodes
+			 char[] message = ip.getHostAddress().toCharArray();
+			 int size = MPI.COMM_WORLD.Size();
+			 for (int i=1; i < size; i++){
+				 MPI.COMM_WORLD.Send(message, 0, message.length, MPI.CHAR, i, 99);	
+			 }	 
+		} catch (MPIException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Start Datanode and Node manager
+	 * @param rank Node ID that is responsible for
+	 */
+	public void startSlaves(int rank){
+		try {
+			// Wait for master node
+			System.out.println("Slave node " + rank + " is waiting");
+			char[] message = new char[20]; 
+			int master = 0;
+			MPI.COMM_WORLD.Recv(message, 0, 20, MPI.CHAR, master, 99);
+			
+			String masterAddress = String.valueOf(message).trim();
+			generateCode(rank, masterAddress);
+			System.out.println("Slave node " + rank + " is starting");
+			
+			// Start slave nodes
+			if (rank <= Configure.NUMBER_OF_DATANODE){
+				runCommand(Deploy.HADOOP_FOLDER + rank + "/sbin/hadoop-daemon.sh start datanode");
+			}else{	
+				runCommand(Deploy.HADOOP_FOLDER + rank + "/sbin/yarn-daemon.sh start nodemanager");
+			}
+			System.out.println("Start slave node " + rank + " --> OK");
+		} catch (MPIException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}		
+	}
+	
+	/**
+	 * Replace the Hadoop source code for each corresponding type of node
+	 * @param rank
+	 * @param masterAddress
+	 */
+	public void generateCode(int rank, String masterAddress){
+		try {
+			File folder = new File(Deploy.DATA_FOLDER + rank);
+			if (!folder.exists()){
+				runCommand("mkdir " + DATA_FOLDER + rank);
+				runCommand("mkdir " + TMP_FOLDER + rank);
+			}
+			
+			runCommand("tar -zxf " + HADOOP_TAR_GZ_FILE + " --directory=" + Deploy.HADOOP_FOLDER + " --transform s/hadoop/" + rank + "/");
+			String HADOOP_INSTALL = Deploy.HADOOP_FOLDER + rank;
+			
+			runCommand("sed -i.bak 's/MRHPC_JAVA_HOME/" + Configure.JAVA_HOME.replaceAll("/", "\\\\/") + "/g' " + HADOOP_INSTALL + "/etc/hadoop/hadoop-env.sh");
+			String HADOOP_CONF_DIR = HADOOP_INSTALL + "/etc/hadoop";
+			runCommand("sed -i.bak 's/MRHPC_HADOOP_CONF_DIR/" + HADOOP_CONF_DIR.replaceAll("/", "\\\\/") + "/g' " + HADOOP_INSTALL + "/etc/hadoop/hadoop-env.sh");						
+			runCommand("sed -i.bak 's/MRHPC_HADOOP_INSTALL/" + HADOOP_INSTALL.replaceAll("/", "\\\\/") + "/g' " + HADOOP_INSTALL + "/etc/hadoop/hadoop-env.sh");
+			
+			runCommand("sed -i.bak 's/MRHPC_MASTER/" + masterAddress + "/g' " + HADOOP_INSTALL + "/etc/hadoop/core-site.xml");
+			runCommand("sed -i.bak 's/MRHPC_USERNAME/" + Configure.USERNAME + "/g' " + HADOOP_INSTALL + "/etc/hadoop/core-site.xml");
+			runCommand("sed -i.bak 's/MRHPC_TMP_FOLDER/" + TMP_FOLDER.replaceAll("/", "\\\\/") + rank + "/g' " + HADOOP_INSTALL + "/etc/hadoop/core-site.xml");
+			
+			runCommand("sed -i.bak 's/MRHPC_DATA_FOLDER/" + DATA_FOLDER.replaceAll("/", "\\\\/") + rank + "/g' " + HADOOP_INSTALL + "/etc/hadoop/hdfs-site.xml");
+			runCommand("sed -i.bak 's/MRHPC_USERNAME/" + Configure.USERNAME + "/g' " + HADOOP_INSTALL + "/etc/hadoop/hdfs-site.xml");
+		
+			runCommand("sed -i.bak 's/MRHPC_JAVA_HOME/" + Configure.JAVA_HOME.replaceAll("/", "\\\\/") + "/g' " + HADOOP_INSTALL + "/etc/hadoop/yarn-env.sh");
+			runCommand("sed -i.bak 's/MRHPC_MASTER/" + masterAddress + "/g' " + HADOOP_INSTALL + "/etc/hadoop/yarn-site.xml");
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}		
+	}
+	
+	/**
+	 * Call bash command
+	 * @param command
+	 * @throws IOException
+	 * @throws InterruptedException
+	 */
+	public static void runCommand(String command)
+			throws IOException, InterruptedException {		
+		ProcessBuilder processBuilder = new ProcessBuilder("/bin/sh", "-c", command);
+		Process process = processBuilder.start();
+
+		InputStream stderr = process.getErrorStream();
+		InputStreamReader isr = new InputStreamReader(stderr);
+		BufferedReader br = new BufferedReader(isr);
+		String line;
+		while ((line = br.readLine()) != null){
+			System.out.println(line);
+		}
+		process.waitFor();
+	}		
+	
+	public static void main(String[] args) throws MPIException {
+		MPI.Init(args);
+		new Deploy();
+		if (MPI.COMM_WORLD.Rank() == 0) {
+			System.out.println("Hadoop is READY!!!");
+			try {
+				String time[] = Configure.ELAPSED_TIME.split(":");
+				long elapse = Long.parseLong(time[0])*60*60*1000 + Long.parseLong(time[1])*60*1000 + Long.parseLong(time[2])*1000;
+				Thread.sleep(elapse);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		MPI.Finalize();
+	}
+}
---
+++ mrhpc/Hadoop-Deploy-FX10/src/csg/chung/mrhpc/fx10/deploy/Generate.java
@@ -0,0 +1,95 @@
+package csg.chung.mrhpc.fx10.deploy;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class Generate {
+		
+	public final static String OPENMPI_JAVA_LIB 		= Configure.DEPLOY_FOLDER + "/openmpi.tar.gz";
+	
+	/**
+	 * This class generates a job .sh file
+	 * 
+	 */
+	public Generate(){
+		try {
+			FileWriter fw = new FileWriter(new File("hadoop.sh"));
+			BufferedWriter out = new BufferedWriter(fw);
+			
+			out.write("#!/bin/sh");
+			out.write("\n");
+			out.write("\n");
+			
+			out.write("#PJM -L \"rscgrp=short\"");
+			out.write("\n");
+			out.write("#PJM -L \"elapse=" + Configure.ELAPSED_TIME + "\"");
+			out.write("\n");
+			out.write("#PJM -L \"node=" + Configure.NUMBER_OF_NODE + "\"");
+			out.write("\n");
+			out.write("#PJM -L \"proc-crproc=512\"");
+			out.write("\n");
+			out.write("#PJM --mpi \"rank-map-bynode\"");
+			out.write("\n");
+			out.write("\n");
+			
+			runCommand("tar -zxf " + OPENMPI_JAVA_LIB + " --directory=" + Configure.DEPLOY_FOLDER);
+			out.write("export JAVA_HOME=" + Configure.JAVA_HOME);
+			out.write("\n");
+			out.write("export OMPIJ_HOME=" + Configure.DEPLOY_FOLDER + "/openmpi");
+			out.write("\n");
+			out.write("export CLASSPATH=.:${OMPIJ_HOME}/lib/mpi.jar");
+			out.write("\n");
+			out.write("export LD_LIBRARY_PATH=.:${OMPIJ_HOME}/lib:${LD_LIBRARY_PATH}");
+			out.write("\n");
+			out.write("export PATH=${OMPIJ_HOME}/bin:${JAVA_HOME}/bin:${PATH}");
+			out.write("\n");
+			out.write("module switch TCSuite/GM-1.2.1-07");
+			out.write("\n");
+			out.write("\n");
+
+			out.write("javac " + Configure.DEPLOY_FOLDER + "/Deploy.java");
+			out.write("\n");
+			out.write("export CLASSPATH=.:$CLASSPATH:" + Configure.DEPLOY_FOLDER);
+			out.write("\n");
+			out.write("mpirun java Deploy");
+			out.write("\n");
+			
+			out.close();
+			fw.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Call bash command
+	 * @param command
+	 * @throws IOException
+	 * @throws InterruptedException
+	 */
+	public static void runCommand(String command) throws IOException,
+			InterruptedException {
+		ProcessBuilder processBuilder = new ProcessBuilder("/bin/sh", "-c",
+				command);
+		Process process = processBuilder.start();
+
+		InputStream stderr = process.getErrorStream();
+		InputStreamReader isr = new InputStreamReader(stderr);
+		BufferedReader br = new BufferedReader(isr);
+		String line;
+		while ((line = br.readLine()) != null) {
+			System.out.println(line);
+		}
+		process.waitFor();
+	}
+
+	public static void main(String[] args){
+		new Generate();
+	}
+}

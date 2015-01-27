e825a1516fb15a46a4a8c8e4d712ac8e5c0bcf59
chung <chung.dao88@gmail.com>
2015-01-27 11:53:43 +0900

    Move Tsubame deployment to processpool package

---
+++ mrhpc/Hadoop-Deploy/src/csg/chung/mrhpc/processpool/Tsubame.java
@@ -0,0 +1,27 @@
+package csg.chung.mrhpc.processpool;
+
+import mpi.MPI;
+import mpi.MPIException;
+import csg.chung.mrhpc.processpool.Configure;
+import csg.chung.mrhpc.processpool.FX10;
+
+public class Tsubame{
+
+	public static void main(String[] args) throws MPIException {
+		MPI.Init(args);
+		new FX10();
+		
+		if (MPI.COMM_WORLD.getRank() == 0) {
+			System.out.println("Hadoop is READY!!!");
+			try {
+				String time[] = Configure.ELAPSED_TIME.split(":");
+				long elapse = Long.parseLong(time[0])*60*60*1000 + Long.parseLong(time[1])*60*1000 + Long.parseLong(time[2])*1000;
+				Thread.sleep(elapse);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		MPI.Finalize();
+	}	
+}

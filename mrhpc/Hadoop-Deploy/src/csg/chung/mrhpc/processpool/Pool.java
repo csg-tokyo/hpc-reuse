d214839d3ba8120e7aade892f95349752890a550
chung <chung.dao88@gmail.com>
2015-11-09 14:47:26 +0900

    Important bug: guarantee that there is no two AppMasters running on the same slot

--- mrhpc/Hadoop-Deploy/src/csg/chung/mrhpc/processpool/Pool.java
+++ mrhpc/Hadoop-Deploy/src/csg/chung/mrhpc/processpool/Pool.java
@@ -10,13 +10,16 @@ public class Pool {
 	public static final int NO_AVAILABLE_SLOT = -1;
 	private int rank;
 	private int[] busyFlag;
+	private int[] amFlag; // Flag for appMaster checking, two appMaster cannot run on the same slot
 	private int counter;
 	
 	public Pool(int rank){
 		this.rank = rank;
 		busyFlag = new int[Configure.NUMBER_PROCESS_EACH_NODE];
+		amFlag = new int[Configure.NUMBER_PROCESS_EACH_NODE];
 		for (int i=0; i < busyFlag.length; i++){
 			busyFlag[i] = 0;
+			amFlag[i] = 0;
 		}
 		busyFlag[0] = 1;
 		counter = 0;
@@ -48,16 +51,20 @@ public class Pool {
 	}	
 	
 	public void startNewProcess(String arg1, String arg2, String arg3){
-		int des = getFreeSLot();
+		int des = getFreeSLot(arg2);
 		if (des != NO_AVAILABLE_SLOT){
 			request(Lib.buildCommand(arg1, arg2, arg3), des);
 		}
 		busyFlag[des] = 1;
+		// Check AppMaster
+		if (arg2.endsWith("000001")){
+			amFlag[des] = 1;
+		}
 	}
 	
-	public int getFreeSLot(){
+	public int getFreeSLot(String containerID){
 		for(;;){
-			if (busyFlag[counter] == 0){
+			if (busyFlag[counter] == 0 && (amFlag[counter] == 0 || (!containerID.endsWith("000001")))){
 				return counter;
 			}else{
 				counter = (counter+1) % busyFlag.length;
